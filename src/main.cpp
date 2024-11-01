881a0565b89eab738dddab59a2d1fb35fffde13c
mgarbowski <mikolaj.garbowski@gmail.com>
2024-11-01 19:07:28 +0100

    move Simulation class to a separate file
    rename variables

--- src/main.cpp
+++ src/main.cpp
@@ -1,105 +1,33 @@
 #define IMGUI_DEFINE_MATH_OPERATORS
-#include <string>
-#include <iostream>
-#include <iomanip>
+#include "simulation/Simulation.h"
+
+#include <SFML/Graphics.hpp>
 #include <box2d/box2d.h>
 #include <imgui-SFML.h>
 #include <imgui.h>
-#include <SFML/Graphics.hpp>
+#include <iostream>
+#include <string>
 
-const int window_width = 1600;
-const int window_height = 1000;
+const int WINDOW_WIDTH = 1600;
+const int WINDOW_HEIGHT = 1000;
 
-class Simulation {
-public:
-  Simulation() : timeStep(1.0f / 60.0f), subStepCount(4) {
-    b2WorldDef worldDef = b2DefaultWorldDef();
-    worldDef.gravity = b2Vec2{0.0f, -10.0f};
-    this->worldId = b2CreateWorld(&worldDef);
-
-    b2BodyDef groundBodyDef = b2DefaultBodyDef();
-    groundBodyDef.position = b2Vec2{0.0f, -10.0f};
-    this->groundId = b2CreateBody(worldId, &groundBodyDef);
-    b2Polygon groundBox = b2MakeBox(50.0f, 10.0f);
-    b2ShapeDef groundShapeDef = b2DefaultShapeDef();
-    b2CreatePolygonShape(groundId, &groundShapeDef, &groundBox);
-
-    b2BodyDef bodyDef = b2DefaultBodyDef();
-    bodyDef.type = b2_dynamicBody;
-    bodyDef.position = b2Vec2{0.0f, 10.0f};
-    this->bodyId = b2CreateBody(worldId, &bodyDef);
-    b2Polygon dynamicBox = b2MakeBox(1.0f, 1.0f);
-    b2ShapeDef shapeDef = b2DefaultShapeDef();
-    shapeDef.density = 1.0f;
-    shapeDef.friction = 0.3f;
-    b2CreatePolygonShape(bodyId, &shapeDef, &dynamicBox);
-  }
-
-  ~Simulation() {
-    b2DestroyWorld(worldId);
-  }
-
-  void step() {
-    b2World_Step(worldId, timeStep, subStepCount);
-  }
-
-  b2Vec2 getBodyPosition() const {
-    return b2Body_GetPosition(bodyId);
-  }
-
-  b2Vec2 getGroundPosition() const {
-    return b2Body_GetPosition(groundId);
-  }
-
-  b2Vec2 getGroundDimensions() const {
-    return getDimensions(groundId);
-  }
-
-  b2Vec2 getBodyDimensions() const {
-    return getDimensions(bodyId);
-  }
-
-  void kickBox() {
-    b2Body_ApplyForce(bodyId, {100.0f, 5000.0f}, getBodyPosition(), true);
-  }
-
-private:
-  b2Vec2 getDimensions(b2BodyId bodyId) const {
-    std::vector<b2ShapeId> shapeIds(5);
-    auto nShapes = b2Body_GetShapes(bodyId, shapeIds.data(), shapeIds.size());
-    assert(nShapes == 1);
-
-    auto polygon = b2Shape_GetPolygon(shapeIds[0]);
-    assert(polygon.count == 4);
-
-    auto width = polygon.vertices[1].x * 2;
-    auto height = polygon.vertices[2].y * 2;
-    return {width, height};
-  }
-
-  b2WorldId worldId;
-  b2BodyId groundId;
-  b2BodyId bodyId;
-  float timeStep;
-  int subStepCount;
-};
 
 void drawSimulation(sf::RenderWindow& window, const Simulation& simulation, sf::Color bodyColor) {
-    b2Vec2 groundPos = simulation.getGroundPosition();
-    b2Vec2 groundSize = simulation.getGroundDimensions();
-    b2Vec2 bodyPos = simulation.getBodyPosition();
-    b2Vec2 bodySize = simulation.getBodyDimensions();
+    b2Vec2 ground_pos = simulation.getGroundPosition();
+    b2Vec2 ground_size = simulation.getGroundDimensions();
+    b2Vec2 body_pos = simulation.getBodyPosition();
+    b2Vec2 body_size = simulation.getBodyDimensions();
 
-    const float scale = 30.0f;
+    const float SCALE = 30.0f;
 
-    sf::RectangleShape ground(sf::Vector2f(groundSize.x * scale, groundSize.y * scale));
-    ground.setOrigin(groundSize.x * scale / 2, groundSize.y * scale / 2);
-    ground.setPosition(groundPos.x * scale + window_width / 2, window_height/2 - groundPos.y * scale);
+    sf::RectangleShape ground(sf::Vector2f(ground_size.x * SCALE, ground_size.y * SCALE));
+    ground.setOrigin(ground_size.x * SCALE / 2, ground_size.y * SCALE / 2);
+    ground.setPosition(ground_pos.x * SCALE + WINDOW_WIDTH / 2, WINDOW_HEIGHT/2 - ground_pos.y * SCALE);
     ground.setFillColor(sf::Color::Green);
 
-    sf::RectangleShape body(sf::Vector2f(bodySize.x * scale, bodySize.y * scale));
-    body.setOrigin(bodySize.x * scale / 2, bodySize.y * scale / 2);
-    body.setPosition(bodyPos.x * scale + window_width / 2, window_height/2 - bodyPos.y * scale);
+    sf::RectangleShape body(sf::Vector2f(body_size.x * SCALE, body_size.y * SCALE));
+    body.setOrigin(body_size.x * SCALE / 2, body_size.y * SCALE / 2);
+    body.setPosition(body_pos.x * SCALE + WINDOW_WIDTH / 2, WINDOW_HEIGHT/2 - body_pos.y * SCALE);
     body.setFillColor(bodyColor);
 
     window.draw(ground);
@@ -111,7 +39,7 @@ int main() {
   sf::ContextSettings settings;
   settings.antialiasingLevel = 8;
 
-  auto window = sf::RenderWindow{{window_width, window_height}, "Box2D with ImGui and SFML", sf::Style::Default, settings};
+  auto window = sf::RenderWindow{{WINDOW_WIDTH, WINDOW_HEIGHT}, "Box2D with ImGui and SFML", sf::Style::Default, settings};
   if (!ImGui::SFML::Init(window)) {
     std::cerr << "Failed to initialize ImGui with SFML." << std::endl;
     window.close();
@@ -124,7 +52,7 @@ int main() {
   io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
 
   Simulation sim;
-  sf::Color bodyColor = sf::Color::Blue;
+  sf::Color body_color = sf::Color::Blue;
 
   while (window.isOpen()) {
     for (auto event = sf::Event{}; window.pollEvent(event);) {
@@ -138,11 +66,11 @@ int main() {
     ImGui::SFML::Update(window, delta_time);
 
     ImGui::Begin("Control Panel");
-    static float color[3] = { bodyColor.r / 255.0f, bodyColor.g / 255.0f, bodyColor.b / 255.0f };
+    static float color[3] = { body_color.r / 255.0f, body_color.g / 255.0f, body_color.b / 255.0f };
     if (ImGui::ColorEdit3("Body Color", color)) {
-      bodyColor.r = static_cast<sf::Uint8>(color[0] * 255);
-      bodyColor.g = static_cast<sf::Uint8>(color[1] * 255);
-      bodyColor.b = static_cast<sf::Uint8>(color[2] * 255);
+      body_color.r = static_cast<sf::Uint8>(color[0] * 255);
+      body_color.g = static_cast<sf::Uint8>(color[1] * 255);
+      body_color.b = static_cast<sf::Uint8>(color[2] * 255);
     }
     if (ImGui::Button("Kick")) {
       sim.kickBox();
@@ -150,7 +78,7 @@ int main() {
     ImGui::End();
 
     window.clear();
-    drawSimulation(window, sim, bodyColor);
+    drawSimulation(window, sim, body_color);
     ImGui::SFML::Render(window);
     window.display();
   }
---
+++ src/simulation/Simulation.cc
@@ -0,0 +1,59 @@
+//
+// Created by mgarbowski on 11/1/24.
+//
+
+#include "Simulation.h"
+
+#include <box2d/box2d.h>
+#include <cassert>
+#include <vector>
+
+Simulation::Simulation() : timeStep(1.0f / 60.0f), subStepCount(4) {
+  b2WorldDef world_def = b2DefaultWorldDef();
+  world_def.gravity = b2Vec2{0.0f, -10.0f};
+  this->worldId = b2CreateWorld(&world_def);
+
+  b2BodyDef ground_body_def = b2DefaultBodyDef();
+  ground_body_def.position = b2Vec2{0.0f, -10.0f};
+  this->groundId = b2CreateBody(worldId, &ground_body_def);
+  b2Polygon ground_box = b2MakeBox(50.0f, 10.0f);
+  b2ShapeDef ground_shape_def = b2DefaultShapeDef();
+  b2CreatePolygonShape(groundId, &ground_shape_def, &ground_box);
+
+  b2BodyDef body_def = b2DefaultBodyDef();
+  body_def.type = b2_dynamicBody;
+  body_def.position = b2Vec2{0.0f, 10.0f};
+  this->bodyId = b2CreateBody(worldId, &body_def);
+  b2Polygon dynamic_box = b2MakeBox(1.0f, 1.0f);
+  b2ShapeDef shape_def = b2DefaultShapeDef();
+  shape_def.density = 1.0f;
+  shape_def.friction = 0.3f;
+  b2CreatePolygonShape(bodyId, &shape_def, &dynamic_box);
+}
+Simulation::~Simulation() { b2DestroyWorld(worldId); }
+void Simulation::step() { b2World_Step(worldId, timeStep, subStepCount); }
+b2Vec2 Simulation::getBodyPosition() const {
+  return b2Body_GetPosition(bodyId);
+}
+b2Vec2 Simulation::getGroundPosition() const {
+  return b2Body_GetPosition(groundId);
+}
+b2Vec2 Simulation::getGroundDimensions() const {
+  return getDimensions(groundId);
+}
+b2Vec2 Simulation::getBodyDimensions() const { return getDimensions(bodyId); }
+void Simulation::kickBox() const {
+  b2Body_ApplyForce(bodyId, {100.0f, 5000.0f}, getBodyPosition(), true);
+}
+b2Vec2 Simulation::getDimensions(b2BodyId bodyId) const {
+  std::vector<b2ShapeId> shape_ids(5);
+  auto n_shapes = b2Body_GetShapes(bodyId, shape_ids.data(), shape_ids.size());
+  assert(n_shapes == 1);
+
+  auto polygon = b2Shape_GetPolygon(shape_ids[0]);
+  assert(polygon.count == 4);
+
+  auto width = polygon.vertices[1].x * 2;
+  auto height = polygon.vertices[2].y * 2;
+  return {width, height};
+}
