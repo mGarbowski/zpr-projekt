816b691b440f961dceb43f204ff01f3af68e295a
mgarbowski <mikolaj.garbowski@gmail.com>
2024-11-03 14:36:26 +0100

    link evolution to simulation library

--- src/tests/simulation/SimulationUnitTest.cc
+++ src/tests/simulation/SimulationUnitTest.cc
@@ -1,6 +1,7 @@
 #include <gtest/gtest.h>
 
 #include "Simulation.h"
+#include "Evolution.h"
 
 namespace SimulationUnitTest {
   TEST(SimulationUnitTest, firstTest) {
@@ -9,5 +10,10 @@ namespace SimulationUnitTest {
       sim.step();
     }
     EXPECT_NEAR(sim.getBodyPosition().y, 1.0, 0.01);
+  }
+
+  TEST(SimulationEvolution, testUsingLinkedLibrary) {
+    Evolution ev;
+    EXPECT_EQ(ev.add(2, 2), 4);
   }
 }
