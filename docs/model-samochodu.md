d02d7d471922eb53d08e9ddd7acb9d18bb287654
mgarbowski <mikolaj.garbowski@gmail.com>
2024-11-16 16:50:56 +0100

    make test of CarChassis more readable, add drawing todocs

---
+++ docs/model-samochodu.md
@@ -0,0 +1,7 @@
+# Model samochodu
+
+Samochód jest zamodelowany jako 8 trójkątów o wspólnym wierzchołku środkowym
+
+Numery na trójkątach odpowiadają indeksowi z metody `CarChassis::getTriangle(int idx)`
+
+![Schemat korpusu samochodu](./images/car-chassis.png)
--- src/tests/simulation/CarChassisTest.cc
+++ src/tests/simulation/CarChassisTest.cc
@@ -15,35 +15,63 @@ void printTriangle(const int idx, const b2Polygon& triangle) {
   std::cout << std::endl;
 }
 
-void assertPointsEqual(b2Vec2 a, b2Vec2 b) {
+void assertPointsEqual(const Position a, const b2Vec2 b) {
   ASSERT_FLOAT_EQ(a.x, b.x);
   ASSERT_FLOAT_EQ(a.y, b.y);
 }
 
-
 TEST(CarChassisTest, vertexPositions) {
-  const CarDescription car_description = {{-4, 2}, {0, 1}, {4, 2}, {2, 0}, {4, -2}, {0, -1}, {-4, -2},
-                                    {-2, 0}, 1.0f,   1.0f,   1.0f,   0.5f,    0.5f};
+  const Position center{0, 0};
+  const Position top_left{-4, 2};
+  const Position top{0, 1};
+  const Position top_right{4, 2};
+  const Position right{2, 0};
+  const Position bottom_right{4, -2};
+  const Position bottom{0, -1};
+  const Position bottom_left{-4, -2};
+  const Position left{-2, 0};
 
+  const CarDescription car_description = {top_left, top,         top_right, right, bottom_right,
+                                          bottom,   bottom_left, left,      1.0f,  1.0f,
+                                          1.0f,     0.5f,        0.5f};
 
   const auto world_def = b2DefaultWorldDef();
   const auto world_id = b2CreateWorld(&world_def);
-  const auto car_chassis = CarChassis::create(world_id, {0, 0}, car_description);
-  const auto body_id = car_chassis.bodyId();
-
-  std::vector<b2ShapeId> shape_ids(8);
-  const auto n_shapes = b2Body_GetShapes(body_id, shape_ids.data(), shape_ids.size());
-  ASSERT_EQ(n_shapes, 8);
+  const auto car_chassis = CarChassis::create(world_id, center, car_description);
 
   for (int i = 0; i < 8; ++i) {
-    const auto tri = b2Shape_GetPolygon(shape_ids[i]);
+    const auto tri = car_chassis.getTriangle(i);
     ASSERT_EQ(tri.count, 3);
     printTriangle(i, tri);
   }
 
-  // top left
-  assertPointsEqual(b2Vec2{-4, 2}, b2Shape_GetPolygon(shape_ids[7]).vertices[1]);
-  assertPointsEqual(b2Vec2{-4, 2}, b2Shape_GetPolygon(shape_ids[6]).vertices[2]);
+  assertPointsEqual(top_left, car_chassis.getTriangle(7).vertices[1]);
+  assertPointsEqual(top_left, car_chassis.getTriangle(6).vertices[2]);
+
+  assertPointsEqual(top, car_chassis.getTriangle(6).vertices[1]);
+  assertPointsEqual(top, car_chassis.getTriangle(5).vertices[2]);
+
+  assertPointsEqual(top_right, car_chassis.getTriangle(5).vertices[1]);
+  assertPointsEqual(top_right, car_chassis.getTriangle(4).vertices[2]);
+
+  assertPointsEqual(right, car_chassis.getTriangle(4).vertices[1]);
+  assertPointsEqual(right, car_chassis.getTriangle(3).vertices[2]);
+
+  assertPointsEqual(bottom_right, car_chassis.getTriangle(3).vertices[1]);
+  assertPointsEqual(bottom_right, car_chassis.getTriangle(2).vertices[2]);
+
+  assertPointsEqual(bottom, car_chassis.getTriangle(2).vertices[1]);
+  assertPointsEqual(bottom, car_chassis.getTriangle(1).vertices[2]);
+
+  assertPointsEqual(bottom_left, car_chassis.getTriangle(1).vertices[1]);
+  assertPointsEqual(bottom_left, car_chassis.getTriangle(0).vertices[2]);
+
+  assertPointsEqual(left, car_chassis.getTriangle(0).vertices[1]);
+  assertPointsEqual(left, car_chassis.getTriangle(7).vertices[2]);
+
+  for (int i = 0; i < 8; ++i) {
+    assertPointsEqual(center, car_chassis.getTriangle(i).vertices[0]);
+  }
 }
 
 }  // namespace CarChassisTest
