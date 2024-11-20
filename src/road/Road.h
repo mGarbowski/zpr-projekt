fc3e8623bfa5299e0dfdd53a0b76fa6193f3149b
Michał Łuszczek <michluszcz@gmail.com>
2024-11-20 19:17:05 +0100

    create abstract Road class

--- src/road/Road.h
+++ src/road/Road.h
@@ -2,8 +2,27 @@
 // Created by mgarbowski on 11/3/24.
 //
 
+
 #ifndef ROAD_H
 #define ROAD_H
+
+#include <vector>
+#include <utility>
+
+class Road {
+public:
+    // Virtual destructor for abstract class
+    virtual ~Road() = default;
+
+    Road() = default;
+
+    // Pure virtual function that returns vector of pairs of floats
+    virtual std::vector<std::pair<float, float>> getRoadPoints() const = 0;
+
+    // Delete copy constructor and assignment operator to prevent slicing
+    Road(const Road&) = delete;
+    Road& operator=(const Road&) = delete;
+};
 
 int add(int a, int b);
 
