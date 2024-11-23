5ce03f877622598e7a09a7473c5b70d554e1541c
Michal Luszczek <michluszcz@gmail.com>
2024-11-23 12:30:40 +0100

    Change Road to data class

--- src/road/Road.h
+++ src/road/Road.h
@@ -1,33 +1,15 @@
 //
-// Created by mgarbowski on 11/3/24.
+// Created by michluszcz on 23.11.24.
 //
-
 
 #ifndef ROAD_H
 #define ROAD_H
 
 #include <vector>
-#include <utility>
-
 class Road {
 public:
-    // Virtual destructor for abstract class
-    virtual ~Road() = default;
+    Road(std::vector<std::pair<float, float>> road) : points_(std::move(road)) {}
 
-    Road() = default;
-
-    // Pure virtual function that returns vector of pairs of floats
-    virtual std::vector<std::pair<float, float>> getRoadPoints() const = 0;
-
-    // Delete copy constructor and assignment operator to prevent slicing
-    Road(const Road&) = delete;
-    Road& operator=(const Road&) = delete;
-
-   protected:
-    std::vector<std::pair<float, float>> road_points_;
-
+    std::vector<std::pair<float, float>> points_;
 };
-
-int add(int a, int b);
-
-#endif //ROAD_H
+#endif  // ROAD_H
