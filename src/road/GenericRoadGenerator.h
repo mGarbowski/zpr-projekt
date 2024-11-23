d8cc8ee8e999d5d68b5bb0c0c7917b4471982b0b
Michal Luszczek <michluszcz@gmail.com>
2024-11-23 12:30:59 +0100

    Create generic road generator

---
+++ src/road/GenericRoadGenerator.h
@@ -0,0 +1,30 @@
+//
+// Created by mgarbowski on 11/3/24.
+//
+
+
+#ifndef ROAD_GENERATOR_H
+#define ROAD_GENERATOR_H
+
+#include <utility>
+#include <vector>
+
+#include "Road.h"
+
+class GenericRoadGenerator {
+public:
+    virtual ~GenericRoadGenerator() = default;
+
+    GenericRoadGenerator() = default;
+
+    /// Pure virtual function that returns a Road class
+    virtual Road generateRoad() const = 0;
+
+    // Delete copy constructor and assignment operator to prevent slicing
+    GenericRoadGenerator(const GenericRoadGenerator&) = delete;
+    GenericRoadGenerator& operator=(const GenericRoadGenerator&) = delete;
+};
+
+int add(int a, int b);
+
+#endif //ROAD_GENERATOR_H
