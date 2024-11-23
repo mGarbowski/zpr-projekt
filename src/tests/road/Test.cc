2114568eb57b38e1c12ca56a172c6faee41b3814
Michal Luszczek <michluszcz@gmail.com>
2024-11-23 13:51:00 +0100

    add sample StaticRoadGenerator test

--- src/tests/road/Test.cc
+++ src/tests/road/Test.cc
@@ -1,9 +1,18 @@
 #include <gtest/gtest.h>
 
 #include "Road.h"
+#include "GenericRoadGenerator.h"
+#include "StaticRoadGenerator.h"
 
 namespace TestTest {
   TEST(RoadTest, test) {
     EXPECT_EQ(add(2, 2), 4);
   }
-}
+
+  TEST(RoadTest, StaticRoadGeneration) {
+    GenericRoadGenerator* road_generator = new StaticRoadGenerator();
+    Road road = road_generator->generateRoad();
+    EXPECT_EQ(road.points_[0].first,  0);
+    EXPECT_EQ(road.points_[0].second,  0);
+  }
+  }  // namespace TestTest
