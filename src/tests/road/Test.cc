2befd794b88ee17eeb589e660b1d0edbe065ba97
mgarbowski <mikolaj.garbowski@gmail.com>
2024-11-25 21:35:23 +0100

    remove leftover dummy test

--- src/tests/road/Test.cc
+++ src/tests/road/Test.cc
@@ -1,18 +1,16 @@
 #include <gtest/gtest.h>
 
-#include "Road.h"
 #include "GenericRoadGenerator.h"
+#include "Road.h"
 #include "StaticRoadGenerator.h"
 
 namespace TestTest {
-  TEST(RoadTest, test) {
-    EXPECT_EQ(add(2, 2), 4);
-  }
 
-  TEST(RoadTest, StaticRoadGeneration) {
-    GenericRoadGenerator* road_generator = new StaticRoadGenerator();
-    Road road = road_generator->generateRoad();
-    EXPECT_EQ(road.points_[0].first,  0);
-    EXPECT_EQ(road.points_[0].second,  0);
-  }
-  }  // namespace TestTest
+TEST(RoadTest, StaticRoadGeneration) {
+  GenericRoadGenerator* road_generator = new StaticRoadGenerator();
+  Road road = road_generator->generateRoad();
+  EXPECT_EQ(road.points_[0].first, 0);
+  EXPECT_EQ(road.points_[0].second, 0);
+}
+
+}  // namespace TestTest
