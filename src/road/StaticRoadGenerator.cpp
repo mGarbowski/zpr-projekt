43948ab8eadcfc2559f5dbc4521ac0650ca82f78
Michał Łuszczek <michluszcz@gmail.com>
2024-11-25 15:18:14 +0100

    change position of ground

--- src/road/StaticRoadGenerator.cpp
+++ src/road/StaticRoadGenerator.cpp
@@ -5,15 +5,15 @@
 #include "StaticRoadGenerator.h"
 Road StaticRoadGenerator::generateRoad() const {
   return Road(std::vector<std::pair<float, float>>{
-      {0.0f, 0.0f},
-      {1.0f, 1.0f},
-      {2.0f, 1.0f},
-      {3.0f, 1.0f},
-      {4.0f, 0.0f},
-      {5.0f, 2.0f},
-      {6.0f, 1.0f},
-      {7.0f, 1.0f},
-      {8.0f, 0.0f},
-      {9.0f, 0.0f}
+      {0.0f, -10.0f},
+      {1.0f, -11.0f},
+      {2.0f, -11.0f},
+      {3.0f, -11.0f},
+      {4.0f, -10.0f},
+      {5.0f, -12.0f},
+      {6.0f, -11.0f},
+      {7.0f, -11.0f},
+      {8.0f, -10.0f},
+      {9.0f, -10.0f}
   });
 }
