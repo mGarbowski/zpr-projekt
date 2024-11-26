1f748367f020dce6e2743acee2c89b0689e06f6f
mgarbowski <mikolaj.garbowski@gmail.com>
2024-11-26 17:47:05 +0100

    add Specimen class

---
+++ src/evolution/Specimen.cc
@@ -0,0 +1,40 @@
+//
+// Created by mgarbowski on 11/26/24.
+//
+
+#include "Specimen.h"
+
+Specimen::Specimen(const CarDescription& car_description) {
+  attributes_ = {car_description.topLeft().x,
+                 car_description.topLeft().y,
+                 car_description.top().x,
+                 car_description.top().y,
+                 car_description.topRight().x,
+                 car_description.topRight().y,
+                 car_description.right().x,
+                 car_description.right().y,
+                 car_description.bottomRight().x,
+                 car_description.bottomRight().y,
+                 car_description.bottom().x,
+                 car_description.bottom().y,
+                 car_description.bottomLeft().x,
+                 car_description.bottomLeft().y,
+                 car_description.left().x,
+                 car_description.left().y,
+                 car_description.bodyDensity(),
+                 car_description.rearWheelDensity(),
+                 car_description.frontWheelDensity(),
+                 car_description.rearWheelRadius(),
+                 car_description.frontWheelRadius()};
+}
+std::vector<float> Specimen::attributes() const {
+  return attributes_;
+}
+CarDescription Specimen::carDescription() const {
+  return CarDescription(
+      Position(attributes_[0], attributes_[1]), Position(attributes_[2], attributes_[3]),
+      Position(attributes_[4], attributes_[5]), Position(attributes_[6], attributes_[7]),
+      Position(attributes_[8], attributes_[9]), Position(attributes_[10], attributes_[11]),
+      Position(attributes_[12], attributes_[13]), Position(attributes_[14], attributes_[15]),
+      attributes_[16], attributes_[17], attributes_[18], attributes_[19], attributes_[20]);
+}
---
+++ src/evolution/Specimen.h
@@ -0,0 +1,27 @@
+//
+// Created by mgarbowski on 11/26/24.
+//
+
+#ifndef SPECIMEN_H
+#define SPECIMEN_H
+
+#include <CarDescription.h>
+#include <vector>
+
+/**
+ * @ingroup evolution
+ * @brief Represents a single specimen in the evolving population.
+ */
+class Specimen {
+ public:
+  Specimen(const CarDescription &car_description);
+
+  std::vector<float> attributes() const;
+
+  CarDescription carDescription() const;
+
+ private:
+  std::vector<float> attributes_;
+};
+
+#endif //SPECIMEN_H
