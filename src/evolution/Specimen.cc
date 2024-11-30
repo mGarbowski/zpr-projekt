f31158a8d2832a1c0ec85e20292cc8e87a43b626
mgarbowski <mikolaj.garbowski@gmail.com>
2024-11-30 20:28:32 +0100

    fix conversion between CarDescription and specimen, add unit test

--- src/evolution/Specimen.cc
+++ src/evolution/Specimen.cc
@@ -7,20 +7,16 @@
 Specimen::Specimen(const CarDescription& car_description) {
   attributes_ = {car_description.topLeft().x,
                  car_description.topLeft().y,
-                 car_description.top().x,
                  car_description.top().y,
                  car_description.topRight().x,
                  car_description.topRight().y,
                  car_description.right().x,
-                 car_description.right().y,
                  car_description.bottomRight().x,
                  car_description.bottomRight().y,
-                 car_description.bottom().x,
                  car_description.bottom().y,
                  car_description.bottomLeft().x,
                  car_description.bottomLeft().y,
                  car_description.left().x,
-                 car_description.left().y,
                  car_description.bodyDensity(),
                  car_description.rearWheelDensity(),
                  car_description.frontWheelDensity(),
@@ -36,11 +32,11 @@ std::vector<float> Specimen::attributes() const {
 }
 CarDescription Specimen::carDescription() const {
   return CarDescription(
-      Position(attributes_[0], attributes_[1]), Position(attributes_[2], attributes_[3]),
-      Position(attributes_[4], attributes_[5]), Position(attributes_[6], attributes_[7]),
-      Position(attributes_[8], attributes_[9]), Position(attributes_[10], attributes_[11]),
-      Position(attributes_[12], attributes_[13]), Position(attributes_[14], attributes_[15]),
-      attributes_[16], attributes_[17], attributes_[18], attributes_[19], attributes_[20]);
+      Position(attributes_[0], attributes_[1]), Position(0, attributes_[2]),
+      Position(attributes_[3], attributes_[4]), Position(attributes_[5], 0),
+      Position(attributes_[6], attributes_[7]), Position(0, attributes_[8]),
+      Position(attributes_[9], attributes_[10]), Position(attributes_[11], 0),
+      attributes_[12], attributes_[13], attributes_[14], attributes_[15], attributes_[16]);
 }
 std::vector<float>& Specimen::attributesRef() {
   return attributes_;
--- src/tests/evolution/SpecimenUnitTest.cc
+++ src/tests/evolution/SpecimenUnitTest.cc
@@ -6,13 +6,38 @@
 
 namespace SpecimenUnitTest {
 
-TEST(Specimen, conversionBetweenCarDescriptionAndAttributes) {
-
+TEST(Specimen, twoWayConversion) {
   const auto car_description = givenCarDescription(0);
+
   const auto specimen = Specimen(car_description);
   const auto new_car_description = specimen.carDescription();
 
   ASSERT_EQ(car_description, new_car_description);
 }
 
+TEST(Specimen, convertCarDescriptionToSpecimen) {
+  const auto car_description = givenCarDescription(0);
+  const auto specimen = Specimen(car_description);
+  const auto attributes = specimen.attributes();
+
+  EXPECT_EQ(attributes.size(), 17);
+  EXPECT_EQ(attributes[0], car_description.topLeft().x);
+  EXPECT_EQ(attributes[1], car_description.topLeft().y);
+  EXPECT_EQ(attributes[2], car_description.top().y);
+  EXPECT_EQ(attributes[3], car_description.topRight().x);
+  EXPECT_EQ(attributes[4], car_description.topRight().y);
+  EXPECT_EQ(attributes[5], car_description.right().x);
+  EXPECT_EQ(attributes[6], car_description.bottomRight().x);
+  EXPECT_EQ(attributes[7], car_description.bottomRight().y);
+  EXPECT_EQ(attributes[8], car_description.bottom().y);
+  EXPECT_EQ(attributes[9], car_description.bottomLeft().x);
+  EXPECT_EQ(attributes[10], car_description.bottomLeft().y);
+  EXPECT_EQ(attributes[11], car_description.left().x);
+  EXPECT_EQ(attributes[12], car_description.bodyDensity());
+  EXPECT_EQ(attributes[13], car_description.rearWheelDensity());
+  EXPECT_EQ(attributes[14], car_description.frontWheelDensity());
+  EXPECT_EQ(attributes[15], car_description.rearWheelRadius());
+  EXPECT_EQ(attributes[16], car_description.frontWheelRadius());
 }
+
+}  // namespace SpecimenUnitTest
