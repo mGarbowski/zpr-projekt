8767a881cd5c8dc16a2847bc70b3fe819602edc3
mgarbowski <mikolaj.garbowski@gmail.com>
2024-12-03 13:01:01 +0100

    add visualisation library

---
+++ src/visualisation/VisualisationUtils.cc
@@ -0,0 +1,5 @@
+//
+// Created by mgarbowski on 12/3/24.
+//
+
+#include "VisualisationUtils.h"
---
+++ src/visualisation/VisualisationUtils.h
@@ -0,0 +1,13 @@
+//
+// Created by mgarbowski on 12/3/24.
+//
+
+#ifndef VISUALISATIONUTILS_H
+#define VISUALISATIONUTILS_H
+
+
+
+
+
+
+#endif //VISUALISATIONUTILS_H
