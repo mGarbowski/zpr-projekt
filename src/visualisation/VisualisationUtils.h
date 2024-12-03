caffa680af43b7c443698a6185a79280f34f536e
mgarbowski <mikolaj.garbowski@gmail.com>
2024-12-03 13:25:57 +0100

    add doc comments for VisualisationUtils

--- src/visualisation/VisualisationUtils.h
+++ src/visualisation/VisualisationUtils.h
@@ -10,26 +10,21 @@
 
 #include "CircleRot.h"
 #include "Position.h"
-#include "Rect.h"
-#include "RectRot.h"
 #include "Size.h"
 
 /**
  * @ingroup visualisation
- *
- * Create a matrix for transforming box2d simulation coordinates to SFML window coordinates
+ * @brief Create a matrix for transforming box2d simulation coordinates to SFML window coordinates
  * @param window_width Width of the SFML window
  * @param window_height Height of the SFML window
  * @param scale Scale factor for the transformation
- * @return Transform matrix
+ * @return SFML Transform matrix
  */
 sf::Transform box2dToSFML(int window_width, int window_height, float scale = 30.0f);
 
 /**
  * @ingroup visualisation
- *
- * Convert size to SFML vector
- *
+ * @brief Convert size to SFML vector
  * @param size Size
  * @return SFML vector
  */
@@ -37,20 +32,43 @@ sf::Vector2f asVector(const Size& size);
 
 /**
  * @ingroup visualisation
- *
- * Convert position to SFML vector
- *
+ * @brief Convert position to SFML vector
  * @param position Position
  * @return SFML vector
  */
 sf::Vector2f asVector(const Position& position);
 
+/**
+ * @ingroup visualisation
+ * @brief Create SFML circle shape
+ * @param circle circle description
+ * @param outline_color outline color
+ * @param fill_color fill color
+ * @return SFML circle shape
+ */
 sf::CircleShape createCircle(const CircleRot& circle, sf::Color outline_color,
                              sf::Color fill_color);
 
+/**
+ * @ingroup visualisation
+ * @brief Create SFML line between 2 points
+ * @param start start point in relative coordinates
+ * @param end end point in relative coordinates
+ * @param position offset for the points
+ * @param color color
+ * @return SFML vertex array
+ */
 sf::VertexArray createLine(const b2Vec2& start, const b2Vec2& end, Position position,
                            sf::Color color = sf::Color::White);
 
+/**
+ * @ingroup visualisation
+ * @brief Create SFML triangle shape
+ * @param triangle box2d polygon describing a triagle
+ * @param position offset for the vertices
+ * @param color fill color
+ * @return SFML convex shape
+ */
 sf::ConvexShape createTriangle(const b2Polygon& triangle, Position position,
                                sf::Color color = sf::Color::White);
 
