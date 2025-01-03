1d0304f6044355aba6be13c3e65b98bb0cad844d
mgarbowski <mikolaj.garbowski@gmail.com>
2025-01-03 17:17:22 +0100

    clang format

--- src/tests/common/CarDescriptionUnitTest.cc
+++ src/tests/common/CarDescriptionUnitTest.cc
@@ -9,15 +9,15 @@
 
 namespace CarDescriptionUnitTest {
 
-TEST(CarDescription, createValidDescription) {
-  const Position top_left = {-1, 1};
-  const Position top = {0, 1};
-  const Position top_right = {1, 1};
-  const Position right = {1, 0};
-  const Position bottom_right = {1, -1};
-  const Position bottom = {0, -1};
-  const Position bottom_left = {-1, -1};
-  const Position left = {-1, 0};
+TEST( CarDescription, createValidDescription ) {
+  const Position top_left = { -1, 1 };
+  const Position top = { 0, 1 };
+  const Position top_right = { 1, 1 };
+  const Position right = { 1, 0 };
+  const Position bottom_right = { 1, -1 };
+  const Position bottom = { 0, -1 };
+  const Position bottom_left = { -1, -1 };
+  const Position left = { -1, 0 };
   constexpr float body_density = 1.0f;
   constexpr float rear_wheel_density = 1.0f;
   constexpr float front_wheel_density = 1.0f;
@@ -26,137 +26,136 @@ TEST(CarDescription, createValidDescription) {
 
   const auto car_description = CarDescription(
       top_left, top, top_right, right, bottom_right, bottom, bottom_left, left, body_density,
-      rear_wheel_density, front_wheel_density, rear_wheel_radius, front_wheel_radius);
+      rear_wheel_density, front_wheel_density, rear_wheel_radius, front_wheel_radius );
 
-  EXPECT_EQ(top_left, car_description.topLeft());
-  EXPECT_EQ(top, car_description.top());
-  EXPECT_EQ(top_right, car_description.topRight());
-  EXPECT_EQ(right, car_description.right());
-  EXPECT_EQ(bottom_right, car_description.bottomRight());
-  EXPECT_EQ(bottom, car_description.bottom());
-  EXPECT_EQ(bottom_left, car_description.bottomLeft());
-  EXPECT_EQ(left, car_description.left());
-  EXPECT_EQ(body_density, car_description.bodyDensity());
-  EXPECT_EQ(rear_wheel_density, car_description.rearWheelDensity());
-  EXPECT_EQ(front_wheel_density, car_description.frontWheelDensity());
-  EXPECT_EQ(rear_wheel_radius, car_description.rearWheelRadius());
-  EXPECT_EQ(front_wheel_radius, car_description.frontWheelRadius());
+  EXPECT_EQ( top_left, car_description.topLeft() );
+  EXPECT_EQ( top, car_description.top() );
+  EXPECT_EQ( top_right, car_description.topRight() );
+  EXPECT_EQ( right, car_description.right() );
+  EXPECT_EQ( bottom_right, car_description.bottomRight() );
+  EXPECT_EQ( bottom, car_description.bottom() );
+  EXPECT_EQ( bottom_left, car_description.bottomLeft() );
+  EXPECT_EQ( left, car_description.left() );
+  EXPECT_EQ( body_density, car_description.bodyDensity() );
+  EXPECT_EQ( rear_wheel_density, car_description.rearWheelDensity() );
+  EXPECT_EQ( front_wheel_density, car_description.frontWheelDensity() );
+  EXPECT_EQ( rear_wheel_radius, car_description.rearWheelRadius() );
+  EXPECT_EQ( front_wheel_radius, car_description.frontWheelRadius() );
 }
 
-TEST(CarDescription, throwsIfPointsAreNotConstrainedToXYAxes) {
+TEST( CarDescription, throwsIfPointsAreNotConstrainedToXYAxes ) {
   // top, left, bottom, right points must be constrained to the x and y axes
-  const Position top_left = {-1, 1};
-  const Position top = {0, 1};
-  const Position top_right = {1, 1};
-  const Position right = {1, 0};
-  const Position bottom_right = {1, -1};
-  const Position bottom = {0, -1};
-  const Position bottom_left = {-1, -1};
-  const Position left = {-1, 0};
+  const Position top_left = { -1, 1 };
+  const Position top = { 0, 1 };
+  const Position top_right = { 1, 1 };
+  const Position right = { 1, 0 };
+  const Position bottom_right = { 1, -1 };
+  const Position bottom = { 0, -1 };
+  const Position bottom_left = { -1, -1 };
+  const Position left = { -1, 0 };
   constexpr float body_density = 1.0f;
   constexpr float rear_wheel_density = 1.0f;
   constexpr float front_wheel_density = 1.0f;
   constexpr float rear_wheel_radius = 1.0f;
   constexpr float front_wheel_radius = 1.0f;
 
-  EXPECT_THROW(CarDescription(top_left, Position{0.1, 1}, top_right, right, bottom_right, bottom,
-                              bottom_left, left, body_density, rear_wheel_density,
-                              front_wheel_density, rear_wheel_radius, front_wheel_radius),
-               std::invalid_argument);
+  EXPECT_THROW( CarDescription( top_left, Position{ 0.1, 1 }, top_right, right, bottom_right,
+                                bottom, bottom_left, left, body_density, rear_wheel_density,
+                                front_wheel_density, rear_wheel_radius, front_wheel_radius ),
+                std::invalid_argument );
 
-  EXPECT_THROW(CarDescription(top_left, top, top_right, {1, 0.1}, bottom_right, bottom, bottom_left,
-                              left, body_density, rear_wheel_density, front_wheel_density,
-                              rear_wheel_radius, front_wheel_radius),
-               std::invalid_argument);
+  EXPECT_THROW( CarDescription( top_left, top, top_right, { 1, 0.1 }, bottom_right, bottom,
+                                bottom_left, left, body_density, rear_wheel_density,
+                                front_wheel_density, rear_wheel_radius, front_wheel_radius ),
+                std::invalid_argument );
 
-  EXPECT_THROW(CarDescription(top_left, top, top_right, right, bottom_right, {-0.1, -1},
-                              bottom_left, left, body_density, rear_wheel_density,
-                              front_wheel_density, rear_wheel_radius, front_wheel_radius),
-               std::invalid_argument);
+  EXPECT_THROW( CarDescription( top_left, top, top_right, right, bottom_right, { -0.1, -1 },
+                                bottom_left, left, body_density, rear_wheel_density,
+                                front_wheel_density, rear_wheel_radius, front_wheel_radius ),
+                std::invalid_argument );
 
-  EXPECT_THROW(CarDescription(top_left, top, top_right, right, bottom_right, bottom, bottom_left,
-                              {-1, 0.1}, body_density, rear_wheel_density, front_wheel_density,
-                              rear_wheel_radius, front_wheel_radius),
-               std::invalid_argument);
+  EXPECT_THROW( CarDescription( top_left, top, top_right, right, bottom_right, bottom, bottom_left,
+                                { -1, 0.1 }, body_density, rear_wheel_density, front_wheel_density,
+                                rear_wheel_radius, front_wheel_radius ),
+                std::invalid_argument );
 }
 
-TEST(CarDescription, throwsIfCornerPointsAreNotRestrictedToQuadrants) {
+TEST( CarDescription, throwsIfCornerPointsAreNotRestrictedToQuadrants ) {
   // top_left, top_right, bottom_right, bottom_left points must be restricted to their respective
   // quadrants
-  const Position top_left = {-1, 1};
-  const Position top = {0, 1};
-  const Position top_right = {1, 1};
-  const Position right = {1, 0};
-  const Position bottom_right = {1, -1};
-  const Position bottom = {0, -1};
-  const Position bottom_left = {-1, -1};
-  const Position left = {-1, 0};
+  const Position top_left = { -1, 1 };
+  const Position top = { 0, 1 };
+  const Position top_right = { 1, 1 };
+  const Position right = { 1, 0 };
+  const Position bottom_right = { 1, -1 };
+  const Position bottom = { 0, -1 };
+  const Position bottom_left = { -1, -1 };
+  const Position left = { -1, 0 };
   constexpr float body_density = 1.0f;
   constexpr float rear_wheel_density = 1.0f;
   constexpr float front_wheel_density = 1.0f;
   constexpr float rear_wheel_radius = 1.0f;
   constexpr float front_wheel_radius = 1.0f;
 
-  EXPECT_THROW(CarDescription({0.1, 1}, top, top_right, right, bottom_right, bottom, bottom_left,
-                              left, body_density, rear_wheel_density, front_wheel_density,
-                              rear_wheel_radius, front_wheel_radius),
-               std::invalid_argument);
-  EXPECT_THROW(CarDescription({-1, -0.1}, top, top_right, right, bottom_right, bottom, bottom_left,
-                              left, body_density, rear_wheel_density, front_wheel_density,
-                              rear_wheel_radius, front_wheel_radius),
-               std::invalid_argument);
-  EXPECT_THROW(CarDescription({0.1, -0.1}, top, top_right, right, bottom_right, bottom, bottom_left,
-                              left, body_density, rear_wheel_density, front_wheel_density,
-                              rear_wheel_radius, front_wheel_radius),
-               std::invalid_argument);
+  EXPECT_THROW( CarDescription( { 0.1, 1 }, top, top_right, right, bottom_right, bottom,
+                                bottom_left, left, body_density, rear_wheel_density,
+                                front_wheel_density, rear_wheel_radius, front_wheel_radius ),
+                std::invalid_argument );
+  EXPECT_THROW( CarDescription( { -1, -0.1 }, top, top_right, right, bottom_right, bottom,
+                                bottom_left, left, body_density, rear_wheel_density,
+                                front_wheel_density, rear_wheel_radius, front_wheel_radius ),
+                std::invalid_argument );
+  EXPECT_THROW( CarDescription( { 0.1, -0.1 }, top, top_right, right, bottom_right, bottom,
+                                bottom_left, left, body_density, rear_wheel_density,
+                                front_wheel_density, rear_wheel_radius, front_wheel_radius ),
+                std::invalid_argument );
 
-  EXPECT_THROW(CarDescription(top_left, top, {-0.1, 1}, right, bottom_right, bottom, bottom_left,
-                              left, body_density, rear_wheel_density, front_wheel_density,
-                              rear_wheel_radius, front_wheel_radius),
-               std::invalid_argument);
-  EXPECT_THROW(CarDescription(top_left, top, {1, -0.1}, right, bottom_right, bottom, bottom_left,
-                              left, body_density, rear_wheel_density, front_wheel_density,
-                              rear_wheel_radius, front_wheel_radius),
-               std::invalid_argument);
-  EXPECT_THROW(CarDescription(top_left, top, top_right, {-0.1, -0.1}, bottom_right, bottom,
-                              bottom_left, left, body_density, rear_wheel_density,
-                              front_wheel_density, rear_wheel_radius, front_wheel_radius),
-               std::invalid_argument);
+  EXPECT_THROW( CarDescription( top_left, top, { -0.1, 1 }, right, bottom_right, bottom,
+                                bottom_left, left, body_density, rear_wheel_density,
+                                front_wheel_density, rear_wheel_radius, front_wheel_radius ),
+                std::invalid_argument );
+  EXPECT_THROW( CarDescription( top_left, top, { 1, -0.1 }, right, bottom_right, bottom,
+                                bottom_left, left, body_density, rear_wheel_density,
+                                front_wheel_density, rear_wheel_radius, front_wheel_radius ),
+                std::invalid_argument );
+  EXPECT_THROW( CarDescription( top_left, top, top_right, { -0.1, -0.1 }, bottom_right, bottom,
+                                bottom_left, left, body_density, rear_wheel_density,
+                                front_wheel_density, rear_wheel_radius, front_wheel_radius ),
+                std::invalid_argument );
 
-  EXPECT_THROW(CarDescription(top_left, top, top_right, right, {-0.1, -1}, bottom, bottom_left,
-                              left, body_density, rear_wheel_density, front_wheel_density,
-                              rear_wheel_radius, front_wheel_radius),
-               std::invalid_argument);
-  EXPECT_THROW(CarDescription(top_left, top, top_right, right, {1, 0.1}, bottom, bottom_left, left,
-                              body_density, rear_wheel_density, front_wheel_density,
-                              rear_wheel_radius, front_wheel_radius),
-               std::invalid_argument);
-  EXPECT_THROW(CarDescription(top_left, top, top_right, right, {-0.1, 0.1}, bottom, bottom_left,
-                              left, body_density, rear_wheel_density, front_wheel_density,
-                              rear_wheel_radius, front_wheel_radius),
-               std::invalid_argument);
+  EXPECT_THROW( CarDescription( top_left, top, top_right, right, { -0.1, -1 }, bottom, bottom_left,
+                                left, body_density, rear_wheel_density, front_wheel_density,
+                                rear_wheel_radius, front_wheel_radius ),
+                std::invalid_argument );
+  EXPECT_THROW( CarDescription( top_left, top, top_right, right, { 1, 0.1 }, bottom, bottom_left,
+                                left, body_density, rear_wheel_density, front_wheel_density,
+                                rear_wheel_radius, front_wheel_radius ),
+                std::invalid_argument );
+  EXPECT_THROW( CarDescription( top_left, top, top_right, right, { -0.1, 0.1 }, bottom, bottom_left,
+                                left, body_density, rear_wheel_density, front_wheel_density,
+                                rear_wheel_radius, front_wheel_radius ),
+                std::invalid_argument );
 
-  EXPECT_THROW(CarDescription(top_left, top, top_right, right, bottom_right, bottom, {0.1, -1},
-                              left, body_density, rear_wheel_density, front_wheel_density,
-                              rear_wheel_radius, front_wheel_radius),
-               std::invalid_argument);
-  EXPECT_THROW(CarDescription(top_left, top, top_right, right, bottom_right, bottom, {-1, 0.1},
-                              left, body_density, rear_wheel_density, front_wheel_density,
-                              rear_wheel_radius, front_wheel_radius),
-               std::invalid_argument);
-  EXPECT_THROW(CarDescription(top_left, top, top_right, right, bottom_right, bottom, {0.1, 0.1},
-                              left, body_density, rear_wheel_density, front_wheel_density,
-                              rear_wheel_radius, front_wheel_radius),
-               std::invalid_argument);
+  EXPECT_THROW( CarDescription( top_left, top, top_right, right, bottom_right, bottom, { 0.1, -1 },
+                                left, body_density, rear_wheel_density, front_wheel_density,
+                                rear_wheel_radius, front_wheel_radius ),
+                std::invalid_argument );
+  EXPECT_THROW( CarDescription( top_left, top, top_right, right, bottom_right, bottom, { -1, 0.1 },
+                                left, body_density, rear_wheel_density, front_wheel_density,
+                                rear_wheel_radius, front_wheel_radius ),
+                std::invalid_argument );
+  EXPECT_THROW( CarDescription( top_left, top, top_right, right, bottom_right, bottom, { 0.1, 0.1 },
+                                left, body_density, rear_wheel_density, front_wheel_density,
+                                rear_wheel_radius, front_wheel_radius ),
+                std::invalid_argument );
 }
 
-TEST(CarDescription, randomGeneratesValidDescription) {
-  std::mt19937 gen(42);  // Seed for reproducibility
+TEST( CarDescription, randomGeneratesValidDescription ) {
+  std::mt19937 gen( 42 );  // Seed for reproducibility
 
-  for (int i = 0; i < 100; ++i) {
-    EXPECT_NO_THROW( CarDescription::random(gen));
+  for( int i = 0; i < 100; ++i ) {
+    EXPECT_NO_THROW( CarDescription::random( gen ) );
   }
-
 }
 
 }  // namespace CarDescriptionUnitTest
