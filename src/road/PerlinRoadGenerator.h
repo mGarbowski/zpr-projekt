c77d3a7c9dcafa08c61c8b3fe43a514a59052e64
Michał Łuszczek <michluszcz@gmail.com>
2025-01-08 22:09:43 +0100

    clang format

--- src/road/PerlinRoadGenerator.h
+++ src/road/PerlinRoadGenerator.h
@@ -1,25 +1,31 @@
 /**
-* @ingroup road
-* @brief A class that generates a road using perlin noise.
-* @authors Mikolaj Garbowski, Michal Luszczek
-*/
+ * @ingroup road
+ * @brief A class that generates a road using perlin noise.
+ * @authors Mikolaj Garbowski, Michal Luszczek
+ */
 
 #ifndef EVOLUTION_PERLINROADGENERATOR_H
 #define EVOLUTION_PERLINROADGENERATOR_H
 #include "RoadGenerator.h"
 
-class PerlinRoadGenerator: public RoadGenerator {
+class PerlinRoadGenerator : public RoadGenerator {
  public:
   using Point = std::pair<float, float>;
   /**
    * @brief Constructor for PerlinRoadGenerator.
    * @param length The length of the generated road. Gets scaled with scale_x
-   * @param grid_size The size of the grid for perlin noise generation, higher = more consitent terrain
+   * @param grid_size The size of the grid for perlin noise generation, higher = more consitent
+   * terrain
    * @param layers The number of layers of the perlin noise.
    * @param scale_y Changes the maximum and minimum road height. higher=higher peaks.
    * @param scale_x Stretches out the road horizontally. The higher, the more gentle the slopes
    */
-  PerlinRoadGenerator(int length, int grid_size, int layers, float scale_y, float scale_x): grid_size_(grid_size), length_( length ), layers_(layers), scale_y_(scale_y), scale_x_(scale_x) {};
+  PerlinRoadGenerator( int length, int grid_size, int layers, float scale_y, float scale_x )
+      : grid_size_( grid_size ),
+        length_( length ),
+        layers_( layers ),
+        scale_y_( scale_y ),
+        scale_x_( scale_x ){};
   ~PerlinRoadGenerator() override = default;
 
  private:
