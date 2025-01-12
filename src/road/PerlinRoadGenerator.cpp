1b26af4bfcf10293144eb7bdc39e02bea086d7e4
mgarbowski <mikolaj.garbowski@gmail.com>
2025-01-12 21:43:14 +0100

    refactor PerlinRoadGenerator

--- src/road/PerlinRoadGenerator.cpp
+++ src/road/PerlinRoadGenerator.cpp
@@ -3,41 +3,39 @@
  * @brief Implementation of PerlinRoadGenerator
  * @authors Mikolaj Garbowski, Michal Luszczek
  */
+
 #include "PerlinRoadGenerator.h"
 
 #include <random>
 
-float randomGradient( int x ) {
+float randomGradient() {
   std::random_device rd;
   std::mt19937 gen( rd() );
-  std::uniform_int_distribution<int> dist( 0, 1 );
+  std::uniform_int_distribution dist( 0, 1 );
   return dist( gen ) == 0 ? -1.0 : 1.0;
-  ;
 }
 
-float dotGridGradient( int grid_x, float chosen_point_x ) {
-  float gradient = randomGradient( grid_x );
-
-  float dx = chosen_point_x - grid_x;
-
-  return ( dx * gradient );
+float dotGridGradient( const int grid_x, const float chosen_point_x ) {
+  const float gradient = randomGradient();
+  const float dx = chosen_point_x - grid_x;
+  return dx * gradient;
 }
 
 // quadratic interpolation
-float interpolate( float a0, float a1, float weight ) {
+float interpolate( const float a0, const float a1, const float weight ) {
   return ( a1 - a0 ) * ( 3.0 - 2.0 * weight ) * weight * weight + a0;
 }
 
 /// generate road height at point x
-float perlin( float x ) {
-  int x0 = (int)x;
-  int x1 = x0 + 1;
-  float offset_0 = x - (float)x0;
+float perlin( const float x ) {
+  const int x0 = static_cast<int>( x );
+  const int x1 = x0 + 1;
+  const float offset_0 = x - static_cast<float>( x0 );
   // generate -1 or 1 for each point x0 and x1
 
-  float graident_0 = dotGridGradient( x0, x );
-  float graident_1 = dotGridGradient( x1, x );
-  float interpolated = interpolate( graident_0, graident_1, offset_0 );
+  const float gradient_0 = dotGridGradient( x0, x );
+  const float gradient_1 = dotGridGradient( x1, x );
+  const float interpolated = interpolate( gradient_0, gradient_1, offset_0 );
 
   return interpolated;
 }
@@ -51,7 +49,7 @@ Road PerlinRoadGenerator::generateRoadImpl() const {
     float frequency = 1;
     float amplitude = 1;
     for( int layer_index = 0; layer_index < layers_; ++layer_index ) {
-      height += perlin( (float)x * frequency / grid_size_ ) * amplitude;
+      height += perlin( static_cast<float>( x ) * frequency / grid_size_ ) * amplitude;
       frequency *= 2;
       amplitude *= 0.5;
     }
