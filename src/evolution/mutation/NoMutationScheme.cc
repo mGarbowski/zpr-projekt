50dbf727a47155ff65ee78a0b42645e4a69fd3d6
mgarbowski <mikolaj.garbowski@gmail.com>
2025-01-12 19:17:43 +0100

    add NoMutationScheme for disabling mutation

---
+++ src/evolution/mutation/NoMutationScheme.cc
@@ -0,0 +1,10 @@
+/**
+* @ingroup evolution
+ * @brief Dummy mutation variant that does not mutate the population
+ * @authors Mikolaj Garbowski, Michal Luszczek
+ */
+
+#include "NoMutationScheme.h"
+void NoMutationScheme::doMutateSpecimen( Specimen& specimen ) {
+  // Do nothing
+}
