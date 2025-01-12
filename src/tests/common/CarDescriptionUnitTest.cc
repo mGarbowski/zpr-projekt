6e9e30e781168963fb7d32e6a6a5e3d7f3ca10cd
mgarbowski <mikolaj.garbowski@gmail.com>
2025-01-12 21:56:42 +0100

    add unit test for CarDescription equality

--- src/tests/common/CarDescriptionUnitTest.cc
+++ src/tests/common/CarDescriptionUnitTest.cc
@@ -158,4 +158,35 @@ TEST( CarDescription, randomGeneratesValidDescription ) {
   }
 }
 
+TEST( CarDescription, equalityOperator ) {
+  const Position top_left = { -1, 1 };
+  const Position top = { 0, 1 };
+  const Position top_right = { 1, 1 };
+  const Position right = { 1, 0 };
+  const Position bottom_right = { 1, -1 };
+  const Position bottom = { 0, -1 };
+  const Position bottom_left = { -1, -1 };
+  const Position left = { -1, 0 };
+  constexpr float body_density = 1.0f;
+  constexpr float rear_wheel_density = 1.0f;
+  constexpr float front_wheel_density = 1.0f;
+  constexpr float rear_wheel_radius = 1.0f;
+  constexpr float front_wheel_radius = 1.0f;
+
+  const auto car_description_1 = CarDescription(
+      top_left, top, top_right, right, bottom_right, bottom, bottom_left, left, body_density,
+      rear_wheel_density, front_wheel_density, rear_wheel_radius, front_wheel_radius );
+
+  const auto car_description_2 = CarDescription(
+      top_left, top, top_right, right, bottom_right, bottom, bottom_left, left, body_density,
+      rear_wheel_density, front_wheel_density, rear_wheel_radius, front_wheel_radius );
+
+  const auto car_description_3 = CarDescription(
+      top_left, top, top_right, right, bottom_right, bottom, bottom_left, left, body_density,
+      rear_wheel_density, front_wheel_density, rear_wheel_radius, front_wheel_radius + 1 );
+
+  EXPECT_EQ( car_description_1, car_description_2 );
+  EXPECT_FALSE( car_description_1 == car_description_3 );
+}
+
 }  // namespace CarDescriptionUnitTest
