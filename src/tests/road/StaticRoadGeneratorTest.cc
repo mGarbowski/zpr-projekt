e61593606876459383c0a83f80def1fba4732cd6
mgarbowski <mikolaj.garbowski@gmail.com>
2025-01-12 21:25:19 +0100

    refactor tests for StaticRoadGenerator

--- src/tests/road/StaticRoadGeneratorTest.cc
+++ src/tests/road/StaticRoadGeneratorTest.cc
@@ -8,30 +8,30 @@ using namespace std;
 namespace RoadTest {
 using Point = std::pair<float, float>;
 
-TEST(StaticRoadGenerator, generateWithDefaultConstructor) {
+TEST( StaticRoadGenerator, generateWithDefaultConstructor ) {
   const unique_ptr<RoadGenerator> road_generator = make_unique<StaticRoadGenerator>();
   const Road road = road_generator->generateRoad();
-  EXPECT_EQ(road.points_.size(), 23);
-  EXPECT_EQ(road.points_[0], Point(-11.0, 0.0));
-  EXPECT_EQ(road.points_[1], Point(-7.0, 1.0));
-  EXPECT_EQ(road.points_[2], Point(-6.0, 0.5));
-  EXPECT_EQ(road.points_[3], Point(-5.0, 0.0));
-  EXPECT_EQ(road.points_[4], Point(-4.0, 0.0));
+  EXPECT_EQ( road.points_.size(), 23 );
+
+  for( size_t i = 0; i < road.points_.size() - 1; ++i ) {
+    EXPECT_TRUE( road.points_[i].first < road.points_[i + 1].first );
+  }
 }
 
-TEST(StaticRoadGenerator, generateWithPassedPoints) {
+TEST( StaticRoadGenerator, generateWithPassedPoints ) {
   const std::vector<Point> points = {
-      {-11.0f, 0.0f}, {-7.0f, 1.0f}, {-6.0f, 0.5f}, {-5.0f, 0.0f}, {-4.0f, 0.0f},  {-3.0f, 0.0f},
-      {-2.0f, 0.0f},  {-1.0f, 0.0f}, {0.0f, 0.0f},  {1.0f, 0.0f},  {2.0f, 0.0f},   {3.0f, 0.0f},
-      {4.0f, 0.0f},   {5.0f, 0.0f},  {6.0f, 0.0f},  {7.0f, 0.0f},  {8.0f, 0.0f},   {9.0f, 0.0f},
-      {10.0f, 0.0f},  {11.0f, 0.0f}, {12.0f, 0.0f}, {13.0f, 0.0f}, {100.0f, 0.0f},
+      { -11.0f, 0.0f }, { -7.0f, 1.0f }, { -6.0f, 0.5f },  { -5.0f, 0.0f }, { -4.0f, 0.0f },
+      { -3.0f, 0.0f },  { -2.0f, 0.0f }, { -1.0f, 0.0f },  { 0.0f, 0.0f },  { 1.0f, 0.0f },
+      { 2.0f, 0.0f },   { 3.0f, 0.0f },  { 4.0f, 0.0f },   { 5.0f, 0.0f },  { 6.0f, 0.0f },
+      { 7.0f, 0.0f },   { 8.0f, 0.0f },  { 9.0f, 0.0f },   { 10.0f, 0.0f }, { 11.0f, 0.0f },
+      { 12.0f, 0.0f },  { 13.0f, 0.0f }, { 100.0f, 0.0f },
   };
 
-  const unique_ptr<RoadGenerator> road_generator = make_unique<StaticRoadGenerator>(points);
+  const unique_ptr<RoadGenerator> road_generator = make_unique<StaticRoadGenerator>( points );
   const Road road = road_generator->generateRoad();
-  EXPECT_EQ(road.points_.size(), points.size());
-  for (auto i = 0; i < points.size(); ++i) {
-    EXPECT_EQ(points[i], road.points_[i]);
+  EXPECT_EQ( road.points_.size(), points.size() );
+  for( auto i = 0; i < points.size(); ++i ) {
+    EXPECT_EQ( points[i], road.points_[i] );
   }
 }
 
