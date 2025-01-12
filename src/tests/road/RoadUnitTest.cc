11c74c16e2bcfe8d7eca01b71475b919acf045ed
mgarbowski <mikolaj.garbowski@gmail.com>
2025-01-12 21:48:30 +0100

    add unit test for Road

--- src/tests/road/RoadUnitTest.cc
+++ src/tests/road/RoadUnitTest.cc
@@ -1,1 +1,16 @@
+#include <gtest/gtest.h>
 
+#include "Road.h"
+
+namespace RoadUnitTest {
+using Point = std::pair<float, float>;
+
+TEST( Road, storesPassedPoints ) {
+  const std::vector<Point> points = { { 0, 0 }, { 1, 1 }, { 2, 2 }, { 3, 3 } };
+
+  const Road road( points );
+
+  EXPECT_EQ( road.points_, points );
+}
+
+}  // namespace RoadUnitTest
