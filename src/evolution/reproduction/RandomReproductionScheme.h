b40fce1fd6311aa3d375c77dd3d873c727d1e81f
Michał Łuszczek <michluszcz@gmail.com>
2025-01-13 17:29:33 +0100

    implement RandomReproductionScheme

---
+++ src/evolution/reproduction/RandomReproductionScheme.h
@@ -0,0 +1,26 @@
+/**
+* @ingroup evolution
+* @brief Random reproduction, concrete implementation of ReproductionScheme
+* @authors Mikolaj Garbowski, Michal Luszczek
+*/
+
+#ifndef EVOLUTION_RANDOMREPRODUCTIONSCHEME_H
+#define EVOLUTION_RANDOMREPRODUCTIONSCHEME_H
+#include <random>
+
+#include "ReproductionScheme.h"
+class RandomReproductionScheme : public ReproductionScheme {
+ public:
+  explicit RandomReproductionScheme( std::mt19937 random_engine )
+      : random_engine_( random_engine ) {}
+
+ protected:
+  Population doReproducePopulation( const Population& population,
+                                    std::vector<float> fitness ) override;
+
+ private:
+  std::mt19937 random_engine_;
+
+};
+
+#endif  // EVOLUTION_RANDOMREPRODUCTIONSCHEME_H
