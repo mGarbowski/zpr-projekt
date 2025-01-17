20e28e11b116538d10533144b072b9e936805b2a
Michał Łuszczek <michluszcz@gmail.com>
2025-01-17 23:53:49 +0100

    create NoCrossoverScheme

---
+++ src/evolution/crossover/NoCrossoverScheme.h
@@ -0,0 +1,16 @@
+/**
+ * @ingroup evolution
+ * @brief Dummy crossover variant that does not crossover any specimens
+ * @authors Mikolaj Garbowski, Michal Luszczek
+ */
+
+#ifndef EVOLUTION_NOCROSSOVERSCHEME_H
+#define EVOLUTION_NOCROSSOVERSCHEME_H
+#include "CrossoverScheme.h"
+
+class NoCrossoverScheme {
+ protected:
+  virtual Population doCrossoverPopulation( const Population& population );
+};
+
+#endif  // EVOLUTION_NOCROSSOVERSCHEME_H
