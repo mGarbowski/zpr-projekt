4bf48ce81e8c2fe9c5aaa1f0ef62769e2534ae73
Michał Łuszczek <michluszcz@gmail.com>
2025-01-19 19:24:59 +0100

    fix typo

--- src/common/Docs.h
+++ src/common/Docs.h
@@ -2,5 +2,5 @@
  * @defgroup common Common
  * @brief Common utilities shared by multiple modules.
  *
- * @auhtors Mikolaj Garbowski, Michal Luszczek
+ * @authors Mikolaj Garbowski, Michal Luszczek
  */
